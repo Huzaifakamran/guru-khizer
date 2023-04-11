1b4aabc57028be8dde535b43f58845fbea207c6e
Huzaifa Kamran <huzaifa.2014@hotmail.com>
2023-04-12 03:36:41 +0500

    changes2

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request,jsonify
 import requests
 import json
-
+import os
 app = Flask(__name__)
 
 @app.route('/webhook',methods = ['GET','POST'])
@@ -75,4 +75,5 @@ def updatePhone(data):
 
     return reply
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0',port=port)
