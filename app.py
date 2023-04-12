e60b0e0c3d2d1b81392afa040a7e135f36c01cbc
Huzaifa Kamran <huzaifa.2014@hotmail.com>
2023-04-12 06:21:45 +0500

    make it better

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def webhook():
 def checkCustID(data):
     try:
         id = data['queryResult']['parameters']['id']
-        data = requests.get(f'https://sheetdb.io/api/v1/<api-key>/search?UniqueRef={id}')
+        data = requests.get(f'https://sheetdb.io/api/v1/zwq6ocyskcltz/search?UniqueRef={id}')
         result = data.json()
         print("RESULT IS: ",result)
         if len(result):
@@ -39,11 +39,55 @@ def checkCustID(data):
             quantity = record['Quantity P1']
             print(firstName)
             reply={
-                'fulfillmentText': f"We found your record your details are: \nFirst Name: {firstName}\nPhone: {phone}\nEmail: {email}\nAddress: {address}\nQuantity: {quantity}"
+                'fulfillmentText': f"We found your record your details are: \nFirst Name: {firstName}\nPhone: {phone}\nEmail: {email}\nAddress: {address}\nQuantity: {quantity}",
+                "fulfillmentMessages": [
+                    {
+                        "text": {
+                        "text": [
+                            f"We found your record your details are: \nFirst Name: {firstName}\nPhone: {phone}\nEmail: {email}\nAddress: {address}\nQuantity: {quantity}"
+                        ]
+                        }
+                    },{
+                        "text": {
+                        "text": [
+                            "Please choose from the below options, you will need your unique reference to hand"
+                        ]
+                        }
+                    },
+                    {
+                        "text": {
+                        "text": [
+                            "1) Delivery Date Confirmation\n2) Order Details Confirmation\n3) Update Phone Number\n4) Stop"
+                        ]
+                        }
+                    }
+                    ],
             }
         else:
             reply={
-                'fulfillmentText': "We didn't found any record against your given id."
+                'fulfillmentText': "We didn't found any record against your given id.",
+                "fulfillmentMessages": [
+                    {
+                        "text": {
+                        "text": [
+                           "We didn't found any record against your given id."
+                        ]
+                        }
+                    },{
+                        "text": {
+                        "text": [
+                            "Please choose from the below options, you will need your unique reference to hand"
+                        ]
+                        }
+                    },
+                    {
+                        "text": {
+                        "text": [
+                            "1) Delivery Date Confirmation\n2) Order Details Confirmation\n3) Update Phone Number\n4) Stop"
+                        ]
+                        }
+                    }
+                    ],
             }
     except Exception as e:
         print(e)
@@ -54,7 +98,7 @@ def updatePhone(data):
     try:
         id = data['queryResult']['parameters']['id']
         phone = data['queryResult']['parameters']['phone']   
-        url = f'https://sheet.best/api/sheets/<api-key>/UniqueRef/{id}'
+        url = f'https://sheet.best/api/sheets/6310829d-6acb-4009-90c2-39fe2cdcbbc3/UniqueRef/{id}'
         payload = json.dumps({
             'Phone':phone
         })
@@ -66,11 +110,55 @@ def updatePhone(data):
         result = response.json()
         if not result:
             reply={
-                'fulfillmentText': "Didn't find this unique id in our database please recheck your id"
+                'fulfillmentText': "Didn't find this unique id in our database please recheck your id",
+                "fulfillmentMessages": [
+                    {
+                        "text": {
+                        "text": [
+                           "Didn't find this unique id in our database please recheck your id"
+                        ]
+                        }
+                    },{
+                        "text": {
+                        "text": [
+                            "Please choose from the below options, you will need your unique reference to hand"
+                        ]
+                        }
+                    },
+                    {
+                        "text": {
+                        "text": [
+                            "1) Delivery Date Confirmation\n2) Order Details Confirmation\n3) Update Phone Number\n4) Stop"
+                        ]
+                        }
+                    }
+                    ],
             }
         else:
             reply = {
-                'fulfillmentText': 'Record Updated Successfully'
+                'fulfillmentText': 'Record Updated Successfully',
+                "fulfillmentMessages": [
+                    {
+                        "text": {
+                        "text": [
+                            "Record Updated Successfully"
+                        ]
+                        }
+                    },{
+                        "text": {
+                        "text": [
+                            "Please choose from the below options, you will need your unique reference to hand"
+                        ]
+                        }
+                    },
+                    {
+                        "text": {
+                        "text": [
+                            "1) Delivery Date Confirmation\n2) Order Details Confirmation\n3) Update Phone Number\n4) Stop"
+                        ]
+                        }
+                    }
+                    ],
             }
     except Exception as e:
         print(e)
@@ -80,7 +168,7 @@ def updatePhone(data):
 def deliveryDetails(data):
     try:
         id = data['queryResult']['parameters']['id']
-        data = requests.get(f'https://sheetdb.io/api/v1/<api-key>/search?UniqueRef={id}')
+        data = requests.get(f'https://sheetdb.io/api/v1/zwq6ocyskcltz/search?UniqueRef={id}')
         result = data.json()
         print("RESULT IS: ",result)
         if len(result):
@@ -89,15 +177,75 @@ def deliveryDetails(data):
             print(orderDetails)
             if orderDetails:
                 reply={
-                    'fulfillmentText': f"Your Order is Booked for {orderDetails}"
+                    'fulfillmentText': f"Your Order is Booked for {orderDetails}",
+                "fulfillmentMessages": [
+                    {
+                        "text": {
+                        "text": [
+                            f"Your Order is Booked for {orderDetails}"
+                        ]
+                        }
+                    },
+                    {
+                        "text": {
+                        "text": [
+                            "1) Delivery Date Confirmation\n2) Order Details Confirmation\n3) Update Phone Number\n4) Stop"
+                        ]
+                        }
+                    }
+                    ],
                 }
             else:
                 reply={
-                    'fulfillmentText': "Your Order Has Not Been Booked For Delivery Yet"
+                    'fulfillmentText': "Your Order Has Not Been Booked For Delivery Yet",
+                "fulfillmentMessages": [
+                   {
+                        "text": {
+                        "text": [
+                            "Your Order Has Not Been Booked For Delivery Yet"
+                        ]
+                        }
+                    },{
+                        "text": {
+                        "text": [
+                            "Please choose from the below options, you will need your unique reference to hand"
+                        ]
+                        }
+                    },
+                    {
+                        "text": {
+                        "text": [
+                            "1) Delivery Date Confirmation\n2) Order Details Confirmation\n3) Update Phone Number\n4) Stop"
+                        ]
+                        }
+                    }
+                    ],
                 }
         else:
             reply={
-                'fulfillmentText': "We didn't found any record against your given id."
+                'fulfillmentText': "We didn't found any record against your given id.",
+                "fulfillmentMessages": [
+                    {
+                        "text": {
+                        "text": [
+                            "We didn't found any record against your given id."
+                        ]
+                        }
+                    },{
+                        "text": {
+                        "text": [
+                            "Please choose from the below options, you will need your unique reference to hand"
+                        ]
+                        }
+                    },
+                    {
+                        "text": {
+                        "text": [
+                            "1) Delivery Date Confirmation\n2) Order Details Confirmation\n3) Update Phone Number\n4) Stop"
+                        ]
+                        }
+                    }
+                    ],
             }
     except Exception as e:
         print(e)
